7317a9bff2973333c174b2581f1709933966f146
mohamedsamir759 <133673062+mohamedsamir759@users.noreply.github.com>
2024-10-15 17:22:00 +0300

    Update hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,10 +1,19 @@
-import fire
-
-def hello(name="World"):
-
-  return "Hello %s!" % name
-
-if __name__ == '__main__':
-
-  fire.Fire(hello)
-
+def greet():
+    
+    print("Hello")
+    
+    list = [1, 5, 2, 50, 20]
+    
+    list.sort()
+    
+    print(list)
+    
+    list.insert(0, 10)
+    
+    print(list)
+    
+    list[1] = 100
+    
+    print(list[1])
+    
+greet() 
